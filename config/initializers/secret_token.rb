4fdf64315efb6c11904931651281765fc8a09bcc
sethmbhele <seth@digitalvoodoo.co.za>
2013-10-25 14:39:04 +0200

    Commit 25.10.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thenoticeboard::Application.config.secret_token = '1f16e566d5ba818690203a5b5dd7e68fb56ddaefea76dfd2077f32ca3ab3f5da08773712f92c0f0982cf674a718b4a282932f92316ac12a38ec782541c7b9ebe'
