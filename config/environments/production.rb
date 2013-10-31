7f48ee64d5f8d5ed982af531c86a4d4aeb0f5f19
Jagdish.Barabari <jagdish.barabari@example.com>
2013-10-31 16:01:01 +0530

    Added mail configarations and forgor password screen done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,21 @@ Thenoticeboard::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'thenoticeboard.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'genknooz2@gmail.com',
+  :password             => 'genknooz!',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
+
+
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
