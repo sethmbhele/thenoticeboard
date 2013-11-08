1a5ddf034c4fe3feae7af9d617b00459ae157b9f
Jagdish.Barabari <jagdish.barabari@example.com>
2013-11-08 15:26:02 +0530

    mail config changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,10 @@ Thenoticeboard::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'thenoticeboard.heroku.com' }
+  config.action_mailer.default_url_options = {:host =>'thenoticeboard.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings  = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'gmail.com',
